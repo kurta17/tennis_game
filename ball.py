6f9cc27ed85bc64d2ca2699b439f5a16700aaf34
kurta17 <114169244+kurta17@users.noreply.github.com>
2023-10-11 10:20:37 +0700

    add boll moving

--- ball.py
+++ ball.py
@@ -5,8 +5,14 @@ class Ball(t.Turtle):
         super().__init__()
         self.speed(0)
         self.penup()
+        self.goto(0,0)
         self.shape("circle")
         self.color("white")
+        
 
     def move_ball(self):
+        x = self.xcor()
+        y = self.ycor()
+        
+        self.goto(x-5,y+5)
         self.forward(20)
