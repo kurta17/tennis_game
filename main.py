14805ba604eeb18f946a13c576e9aaf965bec8c3
kurta17 <114169244+kurta17@users.noreply.github.com>
2023-10-11 17:04:11 +0700

    add bounce paddle function

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import turtle as t
 from ball import Ball
 from line import Lines
 from paddle import Paddle
+from score_board import Score_Left
 import time
 
 ball = Ball()
 line = Lines()
-paddle_left = Paddle(-370,0)
-paddle_right = Paddle(370,0)
-
+paddle_left = Paddle(-350,0)
+paddle_right = Paddle(350,0)
+score_l = Score_Left()
 
 screen= t.Screen()
 screen.setup(800,600)
@@ -20,16 +21,13 @@ screen.onkey(paddle_right.move_down, "Down")
 screen.onkey(paddle_left.move_up, "w")
 screen.onkey(paddle_left.move_down, "s")
 
-
-
-
 line.drow_line()
 
 while True:
     screen.update()
-    time.sleep(0.1)
-    if ball.distance(paddle_left) < 20 or ball.distance(paddle_right) < 20:
-        ball.bounce_wall()
+    time.sleep(0.05)
+    if ball.distance(paddle_right) < 50 and ball.xcor() > 320 or ball.distance(paddle_left) < 50 and ball.xcor() < -320:
+        ball.bounce_paddle()
     ball.move_ball()
     if ball.ycor() > 280 or ball.ycor()< - 280:
         ball.bounce_wall()
