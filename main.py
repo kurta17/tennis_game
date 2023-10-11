faf947a32341d209db8808ef131fd63e02d12ecd
kurta17 <114169244+kurta17@users.noreply.github.com>
2023-10-11 20:50:00 +0700

    add score counting

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from ball import Ball
 from line import Lines
 from paddle import Paddle
 from score_board import Score_Left
+from score_board import Score_Right
 import time
 
 ball = Ball()
@@ -10,6 +11,7 @@ line = Lines()
 paddle_left = Paddle(-350,0)
 paddle_right = Paddle(350,0)
 score_l = Score_Left()
+score_r = Score_Right()
 
 screen= t.Screen()
 screen.setup(800,600)
@@ -29,8 +31,20 @@ while True:
     if ball.distance(paddle_right) < 50 and ball.xcor() > 320 or ball.distance(paddle_left) < 50 and ball.xcor() < -320:
         ball.bounce_paddle()
     ball.move_ball()
+    
     if ball.ycor() > 280 or ball.ycor()< - 280:
         ball.bounce_wall()
+    elif ball.xcor() > 400:
+        ball.goto(0,0)
+        score_l.scorel +=1
+        score_l.print_score()
+        
+    elif ball.xcor() < -400:
+        ball.goto(0,0)
+        score_r.scorer += 1
+        score_r.print_score()
+        
+
     
     
     
